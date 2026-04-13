25cf1b6c7e917eb49db491a296640573e92cf0b4
Burdiak <martin.burdiak@mi.unc.edu.ar>
2026-04-13 09:00:13 -0300

    Creados los atributos de la clase TaskItem

--- src/TaskFlow/Models/TaskItem.cs
+++ src/TaskFlow/Models/TaskItem.cs
@@ -6,5 +6,12 @@ namespace TaskFlow.Models
 {
     internal class TaskItem
     {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Responsible { get; set; }
+        public enum Status { NotStarted, InProgress, Completed }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
