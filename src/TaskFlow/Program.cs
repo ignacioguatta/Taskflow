f1640490727f70898fe91839154e43342633b94c
kevinarrascaeta-oss <kevin.arrascaeta@mi.unc.edu.ar>
2026-04-14 14:45:41 -0300

    feat: agrego prueba de cambio de estado en Program.cs

--- src/TaskFlow/Program.cs
+++ src/TaskFlow/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using TaskFlow.Services;
+using TaskFlow.Models;
+using TaskStatus = TaskFlow.Models.TaskStatus;
+
 namespace TaskFlow
 {
     class Program
@@ -8,6 +11,27 @@ namespace TaskFlow
         {
             TaskService taskService = new TaskService();
             taskService.CrearTarea();
+
+            Console.WriteLine("\n-- Tareas actuales --"):
+            foreach (var t in taskService.GetAllTasks())
+            {
+                Console.WriteLine($"ID: {t.Id} | Titulo: {t.Title} | Estado: {t.Status}");
+            }
+
+            Console.WriteLine("\n-- Cambiando estado de la tarea 1 a enProgreso --");
+            bool resultado = taskService.UpdateStatus(1, TaskStatus.EnProgreso);
+
+            if (resultado)
+                Console.WriteLine("Estado actualizado correctamente!");
+            else
+                Console.WriteLine("No se encontró la tarea.");
+            
+            Console.WriteLine("\n-- Estado Final --");
+            foreach (var t in taskService.GetAllTasks())
+            {
+                Console.WriteLine($"ID: {t.Id} | Titulo: {t.Title} | Estado: {t.Status} | Modificado: {t.UpdatedAt}");
+                
+            }
         }
     }
 }
