0abba27b3033325df15a4cbb8a97eb94c5d26b4a
Rodrigo Sarain <amoresconsideratto@gmail.com>
2026-04-21 16:03:13 -0300

    feat: agrego métodos de utilidad en ConsoleHelper

--- src/TaskFlow/Utils/ConsoleHelper.cs
+++ src/TaskFlow/Utils/ConsoleHelper.cs
@@ -1,10 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace TaskFlow.Utils
 {
     internal class ConsoleHelper
     {
+        public static void MostrarTitulo(string titulo)
+        {
+            Console.WriteLine($"\n{'='.ToString().PadRight(40, '=')}");
+            Console.WriteLine($"  {titulo}");
+            Console.WriteLine($"{'='.ToString().PadRight(40, '=')}");
+        }
+
+        public static void MostrarSeparador()
+        {
+            Console.WriteLine("-------------------------------------");
+        }
     }
 }
